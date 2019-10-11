1dd5b58e503b0a50633e412d4fed3081970ba5af
Ricardo Prestes <ricardo@forlogic.net>
2019-10-10 22:32:49 -0300

    Create the tools classes

---
+++ src/MonkeyTools/Tools/ToolsEmail.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace MonkeyTools
+{
+    public static partial class Tools
+    {
+        public static bool IsEmail(string email)
+        {
+            if (string.IsNullOrEmpty(email))
+                return false;
+            var array = email.Split(new string[] { ",", ";", " " }, StringSplitOptions.RemoveEmptyEntries).Select(s => s.Trim(' ', '.')).ToArray();
+            foreach (var s in array)
+                if (!IsMatch(s, @"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$"))
+                    return false;
+            return true;
+        }
+    }
+}
