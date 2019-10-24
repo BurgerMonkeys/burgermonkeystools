749d5683505423b911362f412974401719c24097
Ricardo Prestes <ricardo@forlogic.net>
2019-10-23 21:53:03 -0300

    Creating url validator and strings regex constants

--- src/BurgerMonkeys.Tools/Validators/ValidatorEmail.cs
+++ src/BurgerMonkeys.Tools/Validators/ValidatorEmail.cs
@@ -5,13 +5,18 @@ namespace BurgerMonkeys.Tools
 {
     public static partial class Validator
     {
+        /// <summary>
+        /// Mmethod to validate if the string is in valid email format
+        /// </summary>
+        /// <param name="email">string to validate</param>
+        /// <returns>Returns if the string is in valid email format</returns>
         public static bool IsEmail(this string email)
         {
             if (string.IsNullOrEmpty(email))
                 return false;
             var array = email.Split(new string[] { ",", ";", " " }, StringSplitOptions.RemoveEmptyEntries).Select(s => s.Trim(' ', '.')).ToArray();
             foreach (var s in array)
-                if (!s.IsMatch(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$"))
+                if (!s.IsMatch(RegexConstants.Email))
                     return false;
             return true;
         }
---
+++ src/BurgerMonkeys.Tools/Validators/ValidatorUrl.cs
@@ -0,0 +1,17 @@
+﻿namespace BurgerMonkeys.Tools
+{
+    public static partial class Validator
+    {
+        /// <summary>
+        /// Mmethod to validate if the string is in valid url format
+        /// </summary>
+        /// <param name="url">string to validate</param>
+        /// <returns>Returns if the string is in valid url format</returns>
+        public static bool IsUrl(this string url)
+        {
+            if (string.IsNullOrEmpty(url))
+                return false;
+            return url.IsMatch(RegexConstants.Url);
+        }
+    }
+}
